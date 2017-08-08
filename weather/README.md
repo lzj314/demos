d381bc4961e2812814f86c30c91e0e051f73b09a
lzj314 <lzj314314@163.com>
2017-08-08 11:36:11 +0800

    weather report

---
+++ weather/README.md
@@ -0,0 +1,4 @@
+# WEATHER 
+天气预报（背景会根据天气不同发生变化）
+运用到了jQuery；H5；CSS3；flex布局。
+![Markdown](http://i4.bvimg.com/1949/31112c863f350a32.png)
---
+++ weather/weather.js
@@ -0,0 +1,65 @@
+﻿/**
+ * Created by Administrator on 2017/8/8.
+ */
+$(function () {
+    var city = '';
+    var weather_data;
+    $.ajax({
+        url:'http://jirenguapi.applinzi.com/city.php',
+        method:'GET',
+        success:function (res) {
+            city = res.slice(0,res.length-1);
+            $.ajax({
+                url:'http://jirenguapi.applinzi.com/weather.php',
+                method:'GET',
+                data:{city:city},
+                success:function (res1) {
+                    var obj = JSON.parse(res1).results;
+                    weather_data = obj[0].weather_data;
+                    render(weather_data);
+                }
+            })
+        },
+        error:function () {
+            alert('通信失败');
+        }
+    });
+    function render(data) {
+        var rain = '雨';
+        var nowObj = data[0];
+        var weatherState = nowObj.weather;
+        var weaNowContent = '<li>'+nowObj.date+'</li>'+
+            '<li>'+'<img src="'+nowObj.dayPictureUrl+'">'+'&nbsp;'+'<img src="'+nowObj.nightPictureUrl+'">'+'</li>'+
+            '<li>'+nowObj.weather+'</li>'+
+            '<li>'+nowObj.temperature+'</li>'+
+            '<li>'+nowObj.wind+'</li>'
+        $('.weatherNow').html(weaNowContent);
+        var weaFurContent = '';
+        for(var i=1,len=data.length;i<len;i++){
+            var weaFuture = data[i];
+            weaFurContent+= '<li>'+
+                '<span>'+weaFuture.date+'</span>'+'<p>'+'<img src="'+weaFuture.dayPictureUrl+'">'+
+                '&nbsp;'+ '<img src="'+weaFuture.nightPictureUrl+'">'+'</p>'+
+                '<p>'+weaFuture.temperature+'</p>'+
+                '<p>'+weaFuture.weather+'</p>'+
+                '<p>'+weaFuture.wind+'</p>'+
+                '</li>'
+        }
+        $('.futureWeather').html(weaFurContent);
+        if(weatherState.indexOf(rain) == -1){
+            //没有雨字
+            $('.main').css('backgroundImage','-webkit-linear-gradient(top, rgb(13, 104, 188), rgb(114, 173, 224))');
+            $('.weather_top').css(
+                { background:'url("http://i4.bvimg.com/1949/c443ab74d8c086f3.jpg")',
+                    backgroundSize:'cover'}
+            )
+        }else {
+            //有雨字
+            $('.main').css('backgroundImage','-webkit-linear-gradient(top,rgb(53, 55, 59), rgb(115, 146, 122))')
+            $('.weather_top').css(
+                {background:'url("http://i4.bvimg.com/1949/4de8d0ce4cc66e6a.jpg")',
+                    backgroundSize:'cover'}
+            )
+        }
+    }
+})
